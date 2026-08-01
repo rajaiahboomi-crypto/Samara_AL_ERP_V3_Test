10397cccf1571c84dbffef3f8e867f53919ba20f
rajaiahboomi-crypto <rajaiahboomi@gmail.com>
2026-08-01 16:56:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.SAMARA_CONFIG = {
+  appName: 'Samara Care',
+  basePath: '/Samara_AL_ERP_V3/',
+  supabaseUrl: 'https://askalabwtlrnoodinayq.supabase.co',
+  supabasePublishableKey: 'sb_publishable_MPf0spA1IsJWWR5-ltVAyA_Z_gctBr-',
+  version: '2.4.0'
+};
